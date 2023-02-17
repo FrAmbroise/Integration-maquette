d6970cdd4f14d6ce1db5961d9353bf4b9b17242d
Ambroise Pietu <pietuambroise@gmail.com>
2023-02-17 17:57:45 +0100

    :lipstick: Styled search bar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <input class="search-bar" type="text" placeholder="Search">
+  <input class="search-bar" id="search" name="search" type="text" placeholder="Search">
 </template>
 
 
@@ -7,9 +7,25 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .search-bar{
-  height: 45px;
+  vertical-align: middle;
+  height: 40px;
   border-radius: 12px;
-  color: #1E1E1E;
-  padding: 16px;
+  padding: 8px;
+  background-image: url('@/assets/search.svg');
+    background-size:32px;
+    background-position-x: 8px;
+    background-position-y:center;
+    background-repeat: no-repeat;
+    text-indent: 40px;
+    border: none;
+    color: #1E1E1E;
 }
+::placeholder{
+  color:black;
+  font-family: 'Poppins';
+  font-weight: 400;
+  font-size: 20px;
+  vertical-align: middle;
+}
+
 </style>
